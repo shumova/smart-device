84468b30ef66701288e3f8d2a98789630ab117ac
Schumova Maria <schumova.maria@gmail.com>
2022-08-13 18:53:06 +0300

    Вёрстка всех блоков

--- source/js/main.js
+++ source/js/main.js
@@ -18,29 +18,166 @@ window.addEventListener('DOMContentLoaded', () => {
   window.addEventListener('load', () => {
     initModals();
 
-    document.querySelector('[footer]').classList.remove('footer--has-no-js');
+    // Фокус на поле имени
 
-    const accordionToggles = Array.from(document.querySelectorAll('[accordion-toggle]'));
-    const accordionContents = Array.from(document.querySelectorAll('[accordion-content]'));
+    const modalToggle = document.querySelector('[data-open-modal]');
+    const nameInput = document.querySelector('[data-name-input]');
+    const setFocus = () => nameInput.focus();
 
-    if (accordionToggles && accordionToggles.length > 0 && accordionContents && accordionContents.length > 0) {
-      const removeActiveClass = () => {
-        accordionToggles.forEach((toggle) => toggle.classList.remove('is-active'));
-        accordionContents.forEach((content) => content.removeAttribute('style'));
-      };
-      const updateActiveClass = (i) => {
-        accordionToggles[i].classList.add('is-active');
-        accordionContents[i].style.maxHeight = accordionContents[i].scrollHeight + 'px';
-      };
+    if (nameInput && modalToggle) {
+      modalToggle.addEventListener('click', () => setTimeout(() => setFocus(), 100));
+    }
 
-      for (let i = 0; i < accordionToggles.length; i++) {
-        accordionToggles[i].addEventListener('click', () => {
-          if (accordionToggles[i].classList.contains('is-active')) {
-            removeActiveClass();
-          } else {
-            removeActiveClass();
-            updateActiveClass(i);
+    // Удаление классов отключённого js
+
+    Array.from(document.querySelectorAll('[data-has-no-js]')).forEach((element) => element.classList.remove('has-no-js'));
+
+    // Функции для кнопок "скрыть"/"показать"
+
+    const getPanelHeight = (panel) => {
+      let panelHeight;
+      panelHeight = panel.scrollHeight + 15 + 'px';
+      return panelHeight;
+    };
+
+    const removeActiveClass = (togglesArray, panelsArray) => {
+      togglesArray.forEach((toggle) => {
+        toggle.classList.remove('is-active');
+        toggle.setAttribute('aria-expanded', 'false');
+        toggle.setAttribute('aria-label', 'Показать');
+      });
+      panelsArray.forEach((panel) => {
+        panel.removeAttribute('style');
+        panel.setAttribute('inert', 'true');
+      });
+    };
+
+    const updateActiveClass = (togglesArray, panelsArray, i) => {
+      togglesArray[i].classList.add('is-active');
+      togglesArray[i].setAttribute('aria-expanded', 'true');
+      togglesArray[i].setAttribute('aria-label', 'Скрыть');
+      panelsArray[i].style.maxHeight = panelsArray[i].scrollHeight + 15 + 'px';
+      panelsArray[i].style.paddingBottom = '15px';
+      panelsArray[i].removeAttribute('inert');
+    };
+
+    const setEventListener = (togglesArray, panelsArray) => {
+      if (togglesArray && togglesArray.length > 0 && panelsArray && panelsArray.length > 0) {
+        for (let i = 0; i < togglesArray.length; i++) {
+          if (panelsArray[i].clientHeight === 0) {
+            panelsArray[i].setAttribute('inert', 'true');
           }
+          togglesArray[i].addEventListener('click', () => {
+            if (togglesArray[i].classList.contains('is-active')) {
+              removeActiveClass(togglesArray, panelsArray);
+            } else {
+              removeActiveClass(togglesArray, panelsArray);
+              updateActiveClass(togglesArray, panelsArray, i);
+            }
+          });
+        }
+      }
+    };
+
+    // Кнопка Подробнее
+
+    const moreToggles = Array.from(document.querySelectorAll('[data-more-toggle]'));
+    const moreContents = Array.from(document.querySelectorAll('[data-more-content]'));
+
+    setEventListener(moreToggles, moreContents);
+
+    // Аккордеон в footer
+
+    const accordionToggles = Array.from(document.querySelectorAll('[data-accordion-toggle]'));
+    const accordionContents = Array.from(document.querySelectorAll('[data-accordion-content]'));
+
+    setEventListener(accordionToggles, accordionContents);
+
+    // Ресайз высоты панелей аккордеона, текстовых
+
+    let resizeTimeout;
+    const resizeThrottler = (togglesArray, panelsArray) => {
+      if (!resizeTimeout) {
+        resizeTimeout = setTimeout(() => {
+          resizeTimeout = null;
+          for (let i = 0; i < togglesArray.length; i++) {
+            if (window.innerWidth < 768 && !togglesArray[i].classList.contains('is-active')) {
+              panelsArray[i].setAttribute('inert', 'true');
+            } else {
+              panelsArray[i].removeAttribute('inert');
+            }
+
+            if (togglesArray[i].classList.contains('is-active')) {
+              let newPanelHeight = getPanelHeight(panelsArray[i]);
+              panelsArray[i].style.maxHeight = newPanelHeight;
+            }
+          }
+        }, 66);
+      }
+    };
+
+    const allTogglesArrays = moreToggles.concat(accordionToggles);
+    const allContentsArrays = moreContents.concat(accordionContents);
+
+    window.addEventListener('resize', () => resizeThrottler(allTogglesArrays, allContentsArrays), false);
+
+    // Маска для телефона
+
+    const phoneInputs = document.querySelectorAll('[data-tel-input]');
+
+    if (phoneInputs && phoneInputs.length > 0) {
+
+      let getInputNumbersValue = function (input) {
+        return input.value.replace(/\D/g, '');
+      };
+
+      let onPhoneInput = (e) => {
+        let input = e.target;
+        let inputNumbersValue = getInputNumbersValue(input);
+        let selectionStart = input.selectionStart;
+        let formattedInputValue = '';
+
+        if (!inputNumbersValue) {
+          input.value = '';
+        }
+
+        if (input.value.length !== selectionStart) {
+          if (e.data && /\D/g.test(e.data)) {
+            input.value = inputNumbersValue;
+          }
+          return;
+        }
+
+        let firstNumber = input.value[0] !== '+' ? input.value[0] : '';
+
+        formattedInputValue = input.value = '+7(' + firstNumber;
+        if (inputNumbersValue.length > 1) {
+          formattedInputValue += inputNumbersValue.substring(1, 4);
+        }
+        if (inputNumbersValue.length >= 5) {
+          formattedInputValue += ') ' + inputNumbersValue.substring(4, 7);
+        }
+        if (inputNumbersValue.length >= 8) {
+          formattedInputValue += '-' + inputNumbersValue.substring(7, 9);
+        }
+        if (inputNumbersValue.length >= 10) {
+          formattedInputValue += '-' + inputNumbersValue.substring(9, 11);
+        }
+        input.value = formattedInputValue;
+      };
+
+      let onPhoneKeyDown = function (e) {
+        let inputValue = e.target.value.replace(/\D/g, '');
+        if (e.keyCode === 8 && inputValue.length === 1) {
+          e.target.value = '';
+        }
+      };
+
+      for (let phoneInput of phoneInputs) {
+        phoneInput.addEventListener('keydown', onPhoneKeyDown);
+        phoneInput.addEventListener('input', onPhoneInput, false);
+        phoneInput.addEventListener('paste', (event) => {
+          event.preventDefault();
         });
       }
     }
